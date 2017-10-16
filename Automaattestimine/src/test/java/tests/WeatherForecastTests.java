6065c14d5dff611d3893f8bb66531aba46a6c155
eightyears <banzho12@gmail.com>
2017-10-16 18:22:29 +0300

    new tests and fixes

--- Automaattestimine/src/test/java/tests/WeatherForecastTests.java
+++ Automaattestimine/src/test/java/tests/WeatherForecastTests.java
@@ -2,6 +2,10 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,22 +40,31 @@ public class WeatherForecastTests {
         assertTrue(currentTemperature > -90 && currentTemperature < 60);
     }
 	
-	//this one needs correction
+	//fixed
 	@Test
-    public void isThreeDayWeatherForecastTemperatureReal() throws JSONException {
+    public void areAllThreeDayWeatherForecastTemperaturesReal() throws JSONException {
 		WeatherForecastService wfs = new WeatherForecastServiceImplementation();
         boolean complexBoolean =  true;
         JSONObject reply = wfs.getThreeDayWeatherForecast(CITY_NAME);
         JSONArray nextFiveDays = (JSONArray) reply.get("list");
-        System.out.println(nextFiveDays.length());
+        //System.out.println(nextFiveDays.length());
         for (int i = 0; i < nextFiveDays.length(); ++i) {
             JSONObject info = nextFiveDays.getJSONObject(i);
-            Double hourTemperature = (Double) (((JSONObject) ((JSONObject) info).get("main"))).get("temp");
-            System.out.println(hourTemperature);
-            if(hourTemperature < -90 || hourTemperature > 60) {
-                complexBoolean = false;
-                break;
-            }
+            if ((((JSONObject) ((JSONObject) info).get("main"))).get("temp") instanceof Integer) {
+            	double hourTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) info).get("main"))).get("temp")).intValue();
+            	//System.out.println(hourTemperature);
+            	if(hourTemperature < -90 || hourTemperature > 60) {
+                    complexBoolean = false;
+                    break;
+                }
+            }else {
+            	Double hourTemperature = (Double) (((JSONObject) ((JSONObject) info).get("main"))).get("temp");
+               // System.out.println(hourTemperature);
+                if(hourTemperature < -90 || hourTemperature > 60) {
+                    complexBoolean = false;
+                    break;
+                }
+            }                       
         }
         assertTrue(complexBoolean);
     }
@@ -63,6 +76,156 @@ public class WeatherForecastTests {
         assertTrue(((JSONObject) reply.get("coord")).get("lon") instanceof Double && ((JSONObject) reply.get("coord")).get("lat") instanceof Double);
     }
 	
-	//soon here will come some new tests
+	@Test
+    public void isWeatherForecastTemperatureGivenPerEveryThreeHours() throws JSONException {
+        WeatherForecastService wfs = new WeatherForecastServiceImplementation();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        boolean complexBoolean = true;
+        JSONObject reply = wfs.getThreeDayWeatherForecast(CITY_NAME);
+        JSONArray nextFiveDays = (JSONArray) reply.get("list");
+        String previousTime = "";
+        String currentTime = "";
+        for (int i = 0; i < nextFiveDays.length(); ++i) {
+            JSONObject info = nextFiveDays.getJSONObject(i);
+            currentTime = (String) ((JSONObject) info).get("dt_txt");
+            //System.out.println(currentTime);
+            if (i != 0) {
+                LocalDateTime  current = LocalDateTime .parse(currentTime, formatter);
+                LocalDateTime  previous = LocalDateTime.parse(previousTime, formatter);
+                long hoursPerStep = ChronoUnit.HOURS.between(previous, current);
+                complexBoolean = hoursPerStep == 3;
+
+                if (!complexBoolean) {
+                    break;
+                }
+            } else {
+                i = 0;
+            }
+            previousTime = currentTime;
+        }
+        
+        assertTrue(complexBoolean);
+    }
+	
+	@Test
+    public void isEveryDayForecastHasMinMaxTemp() throws JSONException {
+		WeatherForecastService wfs = new WeatherForecastServiceImplementation();
+        boolean complexBoolean = true;
+        JSONObject reply = wfs.getThreeDayWeatherForecast(CITY_NAME);
+        JSONArray nextFiveDays = (JSONArray) reply.get("list");
+        for (int i = 0; i < nextFiveDays.length(); ++i) {
+            JSONObject info = nextFiveDays.getJSONObject(i);
+            JSONObject nextThreeHoursForecast = (JSONObject) info;
+            JSONObject nextThreeHoursForecastMain = (JSONObject) nextThreeHoursForecast.get("main");
+            complexBoolean = nextThreeHoursForecastMain.has("temp_min") && nextThreeHoursForecastMain.has("temp_max");
+            if(!complexBoolean) {
+                break;
+            }
+        }
+        assertTrue(complexBoolean);
+    }
+	
+	@Test
+    public void areAllMinMaxTemperaturesCorrect() throws JSONException {
+		WeatherForecastService wfs = new WeatherForecastServiceImplementation();
+        boolean complexBoolean = true;
+        JSONObject reply = wfs.getThreeDayWeatherForecast(CITY_NAME);
+        JSONArray nextFiveDays = (JSONArray) reply.get("list");
+        for (int i = 0; i < nextFiveDays.length(); ++i) {
+            JSONObject info = nextFiveDays.getJSONObject(i);
+            JSONObject nextThreeHoursForecast = (JSONObject) info;
+            if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Integer) {
+            	double minTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_min")).intValue();
+            	if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Double) {
+            		Double maxTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max");
+            		//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            		if(minTemperature < -90 || minTemperature > 60 || maxTemperature < -90 || maxTemperature > 60) {
+                        complexBoolean = false;
+                        break;
+                    }
+            	}           	
+            }else if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Integer) {
+            	double maxTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_max")).intValue();
+            	if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Double) {
+            		Double minTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min");
+            		//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            		if(minTemperature < -90 || minTemperature > 60 || maxTemperature < -90 || maxTemperature > 60) {
+                        complexBoolean = false;
+                        break;
+                    }
+            	}     
+            }else if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Integer &&
+            		((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Integer) {
+            	double minTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_min")).intValue();
+            	double maxTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_max")).intValue();
+            	//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            	if(minTemperature < -90 || minTemperature > 60 || maxTemperature < -90 || maxTemperature > 60) {
+            		complexBoolean = false;
+                    break;
+            	}
+            }else {
+            	Double minTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min");
+                Double maxTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max");
+                //System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+                if(minTemperature < -90 || minTemperature > 60 || maxTemperature < -90 || maxTemperature > 60) {
+                    complexBoolean = false;
+                    break;
+                }
+            }            
+        }
+        assertTrue(complexBoolean);
+    }
+	
+	@Test
+    public void areAllMinTemperaturesLessThanMaxTemperatures() throws JSONException {
+		WeatherForecastService wfs = new WeatherForecastServiceImplementation();
+        boolean complexBoolean = true;
+        JSONObject reply = wfs.getThreeDayWeatherForecast(CITY_NAME);
+        JSONArray nextFiveDays = (JSONArray) reply.get("list");
+        for (int i = 0; i < nextFiveDays.length(); ++i) {
+            JSONObject info = nextFiveDays.getJSONObject(i);
+            JSONObject nextThreeHoursForecast = (JSONObject) info;
+            if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Integer) {
+            	double minTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_min")).intValue();
+            	if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Double) {
+            		Double maxTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max");
+            		//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            		if(minTemperature > maxTemperature) {
+                        complexBoolean = false;
+                        break;
+                    }
+            	}           	
+            }else if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Integer) {
+            	double maxTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_max")).intValue();
+            	if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Double) {
+            		Double minTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min");
+            		//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            		if(minTemperature > maxTemperature) {
+                        complexBoolean = false;
+                        break;
+                    }
+            	}     
+            }else if (((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min") instanceof Integer &&
+            		((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max") instanceof Integer) {
+            	double minTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_min")).intValue();
+            	double maxTemperature = (double) ((Integer)(((JSONObject) ((JSONObject) nextThreeHoursForecast).get("main"))).get("temp_max")).intValue();
+            	//System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+            	if(minTemperature > maxTemperature) {
+            		complexBoolean = false;
+                    break;
+            	}
+            }else {
+            	Double minTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_min");
+                Double maxTemperature = (Double) ((JSONObject) nextThreeHoursForecast.get("main")).get("temp_max");
+                System.out.println("MIN: " + minTemperature + " MAX: " + maxTemperature);
+                if(minTemperature > maxTemperature) {
+                    complexBoolean = false;
+                    break;
+                }
+            }            
+        }
+        assertTrue(complexBoolean);
+    }
+
 
 }
